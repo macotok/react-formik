52d3528abd07c3d66771fcee46b5975be1859492
macotok <monochromer@r8.ucom.ne.jp>
2020-08-10 19:35:58 +0900

    implement simple form

---
+++ practice-formik/simple-form/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from 'react';
+import YoutubeForm from './components/YoutubeForm';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <YoutubeForm />
+    </div>
+  );
+}
+
+export default App;
